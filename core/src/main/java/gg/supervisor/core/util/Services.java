27e321b77bfcf375e873fd33c547aa38469017cc
Solo <40409775+Hadimhz@users.noreply.github.com>
2024-09-07 09:50:36 +0300

    `Services#loadIfPresent` now actually loads the class, in case it was unloaded.
    This is useful for API that potentially have supervisor components implemented but are not loaded due to the scope of the reflection scanner.
    
    Uses reflections to get the method as a mean to keep the function `SupervisorLoader#createComponentInstance` hidden from the public which could cause issues if used incorrectly.

--- core/src/main/java/gg/supervisor/core/util/Services.java
+++ core/src/main/java/gg/supervisor/core/util/Services.java
@@ -1,5 +1,11 @@
 package gg.supervisor.core.util;
 
+import gg.supervisor.core.annotation.Component;
+import gg.supervisor.core.loader.SupervisorLoader;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +24,17 @@ public class Services {
     public static <T> T loadIfPresent(Class<T> clazz) {
 
         T instance = (T) registeredServices.get(clazz);
+
+        if (instance == null && clazz.isAnnotationPresent(Component.class)) {
+            try {
+                Method method = SupervisorLoader.class.getDeclaredMethod("createComponentInstance", Class.class, Plugin.class);
+                method.setAccessible(true);
+                return (T) method.invoke(SupervisorLoader.class, clazz, (Plugin) registeredServices.get(Plugin.class));
+
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return instance;
     }
 
