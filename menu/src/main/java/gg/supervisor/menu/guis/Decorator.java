20da579970891d3b3edd559568b9e4d997353157
Solo <40409775+Hadimhz@users.noreply.github.com>
2024-10-27 00:30:57 +0200

    Optimized Decorator and added getSlots(char key)

--- menu/src/main/java/gg/supervisor/menu/guis/Decorator.java
+++ menu/src/main/java/gg/supervisor/menu/guis/Decorator.java
@@ -1,14 +1,11 @@
 package gg.supervisor.menu.guis;
 
 import gg.supervisor.menu.exception.MenuException;
-import gg.supervisor.menu.guis.item.MenuItem;
+import gg.supervisor.menu.item.MenuItem;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 
 @RequiredArgsConstructor
@@ -16,11 +13,16 @@ public class Decorator {
 
     private final BaseGui gui;
 
+    private final Map<Character, List<Integer>> schemaSlots = new HashMap<>();
+
     private final List<String> schema = new ArrayList<>();
 
     public void decorate(String... lines) {
         schema.clear();
         schema.addAll(List.of(Arrays.copyOfRange(lines, 0, gui.getRows())));
+
+        schemaSlots.clear();
+        schemaSlots.putAll(getSchemaSlots());
     }
 
     public void set(char key, MenuItem menuItem) {
@@ -62,50 +64,28 @@ public class Decorator {
         if (schema.isEmpty())
             throw new MenuException("You must decorate this first!");
 
-        for (int i = 0; i < schema.size(); i++) {
-            String line = schema.get(i);
-            char[] chars = line.toCharArray();
+        for (Integer slot : schemaSlots.getOrDefault(key, new ArrayList<>())) {
+            if (slot > gui.getInventory().getSize() - 1)
+                break;
 
-            for (int column = 0; column < chars.length; column++) {
-                char character = chars[column];
-
-                int slot = column + (i * 9);
-
-                if (slot > gui.getInventory().getSize() - 1)
-                    continue;
-
-                if (key == character)
-                    slotConsumer.accept(slot);
-            }
+            slotConsumer.accept(slot);
         }
     }
 
     public void forFirstEmpty(char key, int amount, Consumer<Integer> slotConsumer) {
-
-        int empties = 0;
-
         if (schema.isEmpty())
             throw new MenuException("You must decorate this first!");
 
-        for (int i = 0; i < schema.size(); i++) {
-            String line = schema.get(i);
-            char[] chars = line.toCharArray();
+        int empties = 0;
 
-            for (int column = 0; column < chars.length; column++) {
-                char character = chars[column];
+        for (Integer slot : schemaSlots.getOrDefault(key, new ArrayList<>())) {
+            if (++empties > amount || slot > gui.getInventory().getSize() - 1)
+                break;
 
-                if (character == key) continue;
+            if (gui.getMenuItem(slot) != null || gui.getSlotAction(slot) != null)
+                continue;
 
-                int slot = column + (i * 9);
-
-                if (++empties > amount || slot > gui.getInventory().getSize() - 1)
-                    break;
-
-                if (gui.getMenuItem(slot) != null || gui.getSlotAction(slot) != null)
-                    continue;
-
-                slotConsumer.accept(slot);
-            }
+            slotConsumer.accept(slot);
         }
     }
 
@@ -117,6 +97,19 @@ public class Decorator {
         if (schema.isEmpty())
             throw new MenuException("You must decorate this first!");
 
+        return schemaSlots.getOrDefault(key, new ArrayList<>()).stream().findFirst().orElse(-1);
+    }
+
+    public List<Integer> getSlots(char key) {
+        if (schema.isEmpty())
+            throw new MenuException("You must decorate this first!");
+
+        return schemaSlots.getOrDefault(key, new ArrayList<>());
+    }
+
+    public Map<Character, List<Integer>> getSchemaSlots() {
+        Map<Character, List<Integer>> toReturn = new HashMap<>();
+
         for (int i = 0; i < schema.size(); i++) {
             String line = schema.get(i);
             char[] chars = line.toCharArray();
@@ -124,21 +117,17 @@ public class Decorator {
             for (int column = 0; column < chars.length; column++) {
                 char character = chars[column];
 
-                if (character == key) continue;
-
                 int slot = column + (i * 9);
-
                 if (slot > gui.getInventory().getSize() - 1)
                     break;
 
-                if (gui.getMenuItem(slot) != null || gui.getSlotAction(slot) != null)
-                    continue;
+                toReturn.putIfAbsent(character, new ArrayList<>());
 
-                return slot;
+                toReturn.get(character).add(slot);
             }
         }
 
-        return -1;
+        return toReturn;
     }
 
 }
