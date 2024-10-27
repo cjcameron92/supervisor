1e8aa4b327d6b52578e96a7247277b5857deb267
Solo <40409775+Hadimhz@users.noreply.github.com>
2024-10-27 12:48:59 +0200

    Added Item and Skull Builders

--- menu/build.gradle.kts
+++ menu/build.gradle.kts
@@ -1,4 +1,9 @@
 dependencies {
+    implementation(project(":util"))
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
+}
+
+tasks.shadowJar {
+    minimize()
 }
