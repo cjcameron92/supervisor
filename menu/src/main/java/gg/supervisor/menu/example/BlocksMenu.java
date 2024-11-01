9577b157a26c81ff0ea62b9795784497fae2b3ba
Solo <40409775+Hadimhz@users.noreply.github.com>
2024-11-01 03:10:33 +0200

    Menu Examples

---
+++ menu/src/main/java/gg/supervisor/menu/example/BlocksMenu.java
@@ -0,0 +1,61 @@
+package gg.supervisor.menu.example;
+
+import gg.supervisor.menu.entities.InteractionModifier;
+import gg.supervisor.menu.guis.Pager;
+import gg.supervisor.menu.guis.impl.PersonalizedMenu;
+import gg.supervisor.menu.item.MenuItem;
+import gg.supervisor.util.chat.Text;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class BlocksMenu extends PersonalizedMenu {
+    private static final List<MenuItem> gradient;
+
+    static {
+        gradient = Stream.of(Material.values()).filter(Material::isItem).map(x-> new MenuItem(x,event -> {
+            event.getWhoClicked().closeInventory();
+        })).toList();
+    }
+
+    private final BukkitTask task;
+
+    public BlocksMenu(Plugin plugin, Player player) {
+        super(6, Text.translate(""), InteractionModifier.VALUES, player);
+
+        getDecorator().decorate(
+                "xxxxxxxxx",
+                "xxxxxxxxx",
+                "xxxxxxxxx",
+                "xxxxxxxxx",
+                "xxxxxxxxx",
+                "xxxxxxxxx"
+        );
+
+        final Pager first = new Pager(this, 'x').step(100).endless(Pager.EndlessType.SIMPLE);
+
+        first.add(gradient);
+
+        updateTitle(Text.translate("&cBlocks! &7(" + first.getPage() + "/" + first.getTotalPages() + ")"));
+
+        task = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            first.next();
+
+            updateTitle(Text.translate("&cBlocks! &7(" + first.getPage() + "/" + first.getTotalPages() + ")"));
+        }, 10L, 10L);
+    }
+
+    @Override
+    public void onClose() {
+        super.onClose();
+
+        task.cancel();
+        System.out.println("Cancelled task");
+
+    }
+}
---
+++ menu/src/main/java/gg/supervisor/menu/example/SimpleMenu.java
@@ -0,0 +1,29 @@
+package gg.supervisor.menu.example;
+
+import gg.supervisor.menu.builder.ItemBuilder;
+import gg.supervisor.menu.entities.InteractionModifier;
+import gg.supervisor.menu.guis.impl.PersonalizedMenu;
+import gg.supervisor.util.chat.Text;
+import lombok.val;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+public class SimpleMenu extends PersonalizedMenu {
+
+    public SimpleMenu(Player player) {
+        super(3, Text.translate("&aSimple"), InteractionModifier.VALUES, player);
+    }
+
+    @Override
+    public void redraw() {
+
+        setItem(2, 4, ItemBuilder.from(Material.DIAMOND).name(String.valueOf(System.currentTimeMillis())).menuItem(event -> redraw()));
+
+        setItem(2, 6, ItemBuilder.from(Material.EMERALD).name(String.valueOf(System.currentTimeMillis())).menuItem(event -> {
+            val itemStack = getMenuItem(event.getSlot()).getItemStack();
+
+            updateItem(event.getSlot(), ItemBuilder.from(itemStack).name(String.valueOf(System.currentTimeMillis())).editItem(item -> item.setType(Material.ARROW)).build(string -> string.replace("0", "T")));
+        }));
+
+    }
+}
